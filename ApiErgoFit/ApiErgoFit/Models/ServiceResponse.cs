1e24c55329493cb997df7fb836fb31a53598d7a5
Pablo Pierre <pablo_chips@hotmail.com>
2025-09-06 20:41:25 -0300

    cofigurando service response

--- ApiErgoFit/ApiErgoFit/Models/ServiceResponse.cs
+++ ApiErgoFit/ApiErgoFit/Models/ServiceResponse.cs
@@ -1,6 +1,11 @@
 ﻿namespace ApiErgoFit.Models
 {
-    public class ServiceResponse
+    public class ServiceResponse<T>
     {
+        public T? Dados { get; set; }
+
+        public string Mensagem { get; set; } = string.Empty;
+
+        public bool Sucesso { get; set; } = true;
     }
 }
