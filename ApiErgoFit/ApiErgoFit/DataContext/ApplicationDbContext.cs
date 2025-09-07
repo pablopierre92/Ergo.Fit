dfc54513b852510ef3d8c55cb4188e2a29a774b5
Pablo Pierre <pablo_chips@hotmail.com>
2025-09-06 21:28:01 -0300

    configurando conexao com o banco

---
+++ ApiErgoFit/ApiErgoFit/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using ApiErgoFit.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiErgoFit.DataContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+        }
+
+
+        public DbSet<FuncionarioModel> Funcionarios { get; set; }
+        public DbSet<CategoriaModel> Categorias { get; set; }
+        public DbSet<DepartamentoModel> Departamentos { get; set; }
+        public DbSet<EmpresaModel> Empresas { get; set; }
+        public DbSet<ExercicioModel> Exercicios { get; set; }
+        public DbSet<SessaoModel> Sessao { get; set; }
+        public DbSet<UsuarioMasterModel> UsuariosMaster { get; set; }
+    }
+}
